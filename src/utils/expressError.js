c824814c7deedb64bad3aab2451cf60363324b6c
Arthur Hauser <arthur.hauser@gmail.com>
2024-07-14 18:33:50 -0400

    first commit

---
+++ src/utils/expressError.js
@@ -0,0 +1,11 @@
+//src/utils/expressError.js
+class ExpressError extends Error {
+    constructor(message, status) {
+      super();
+      this.message = message;
+      this.status = status;
+      console.error(this.stack);
+    }
+  }
+  
+  module.exports = ExpressError;
